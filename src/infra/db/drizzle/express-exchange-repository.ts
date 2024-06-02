e8d98ec86798a1a148dc9a3211dd140c5dffba18
tjapa <nicotamalu@gmail.com>
2024-06-02 20:16:31 -0300

    feat: add UpdateExpressExchangeRepository implementation

--- src/infra/db/drizzle/express-exchange-repository.ts
+++ src/infra/db/drizzle/express-exchange-repository.ts
@@ -6,17 +6,24 @@ import {
 import { drizzleClient } from './dizzleClient'
 import { expressExchanges } from './schemas'
 import { GetExpressExchangeByInvoiceIdRepository } from '@/repository/express-exchange/get-express-exchange-by-invoice-id-repository'
-import { and, eq } from 'drizzle-orm'
+import { and, eq, sql } from 'drizzle-orm'
 import { HandleInvalidUuidError } from './decorators/handle-invalid-uuid-error'
 import { GetExpressExchangeByIdRepository } from '@/repository/express-exchange/get-express-exchange-by-id-repository'
 import { DeleteExpressExchangeByIdRepository } from '@/repository/express-exchange/delete-express-exchange-by-id-repository'
+import {
+  UpdateExpressExchangeData,
+  UpdateExpressExchangeRepository,
+} from '@/repository/express-exchange/update-express-exchange-repository'
+import { conflictUpdateSetAllColumns } from './utils/conflict-update-set-all-columns'
 
 export class ExpressExchangeRepository
   implements
-  CreateExpressExchangeRepository,
-  GetExpressExchangeByIdRepository,
-  GetExpressExchangeByInvoiceIdRepository,
-  DeleteExpressExchangeByIdRepository {
+    CreateExpressExchangeRepository,
+    GetExpressExchangeByIdRepository,
+    GetExpressExchangeByInvoiceIdRepository,
+    UpdateExpressExchangeRepository,
+    DeleteExpressExchangeByIdRepository
+{
   @HandleInvalidUuidError
   async create(
     expressExchangeData: CreateExpressExchangeData,
@@ -53,6 +60,29 @@ export class ExpressExchangeRepository
         eq(expressExchanges.customerId, customerId),
       ),
     })
+  }
+
+  @HandleInvalidUuidError
+  async update(
+    expressExchangeData: UpdateExpressExchangeData,
+    expressExchangeId: string,
+    customerId: string,
+  ): Promise<ExpressExchange> {
+    return (
+      await drizzleClient
+        .update(expressExchanges)
+        .set({
+          ...expressExchangeData,
+          updatedAt: sql`CURRENT_TIMESTAMP`,
+        })
+        .where(
+          and(
+            eq(expressExchanges.id, expressExchangeId),
+            eq(expressExchanges.customerId, customerId),
+          ),
+        )
+        .returning()
+    )[0]
   }
 
   @HandleInvalidUuidError
